bea2560dbdf7e6463b1392309476c1bc2cad283f
IAmProgrammist <vladislav.pakhomov@bk.ru>
2025-04-14 00:21:05 +0300

    Completed ARPHelper object code

--- include/webstur/arp.h
+++ include/webstur/arp.h
@@ -5,8 +5,23 @@
 
 class DLLEXPORT ARPHelper {
 public:
-	ARPHelper();
-
+	// Выводит таблицу ARP в указанный ostream
 	static std::ostream& printARPTable(std::ostream& out);
+	// Добавляет запись в ARP таблицу, где
+	//   ip               адрес, строка в формате 255.255.255.255
+	//   address          физический адрес, строка в формате ff:ff:ff:ff:ff:ff
+	//   interface_index  индекс интерфейса 
 	static void addArpEntry(std::string ip, std::string address, int interface_index);
+	// Удаляет запись из ARP таблицы, где
+	//   ip               адрес, строка в формате 255.255.255.255
+	//   address          физический адрес, строка в формате ff:ff:ff:ff:ff:ff
+	//   interface_index  индекс интерфейса 
+	static void deleteArpEntry(std::string ip, std::string address, int interface_index);
+	// Отправляет ARP запрос, где
+	//   find             адрес, который нужно найти
+	static std::string sendARP(std::string find);
+    // Форматирует физический адрес
+	static std::string prettyPrintPhysicalAddress(unsigned char* addr, int addr_size);
+	// Возвращает IP адрес интерфейса по его индексу
+	static std::string getInterfaceIpAddressByIndex(int dw_index);
 };
--- src/webstur/arp.cpp
+++ src/webstur/arp.cpp
@@ -8,12 +8,12 @@
 #include <sstream>
 #include <webstur/arp.h>
 
-std::string prettyPrintPhysicalAddress(unsigned char* addr, int addr_size) {
+std::string ARPHelper::prettyPrintPhysicalAddress(unsigned char* addr, int addr_size) {
     std::ostringstream result;
 
     for (int i = 0; i < addr_size; i++) {
         if (i == addr_size - 1) {
-            result << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)addr[i];
+            result << std::setfill('0') << std::setw(2) << std::hex << (unsigned int) addr[i];
         } else {
             result << std::setfill('0') << std::setw(2) << std::hex << (unsigned int) addr[i] << ":";
         }
@@ -22,64 +22,64 @@ std::string prettyPrintPhysicalAddress(unsigned char* addr, int addr_size) {
     return result.str();
 }
 
-std::string getInterfaceIpAddressByIndex(int dwIndex) {
-    PIP_ADAPTER_ADDRESSES pAddresses = NULL;
-    unsigned long outBufLen = 0;
-    int dwRetVal = 0;
+std::string ARPHelper::getInterfaceIpAddressByIndex(int dwIndex) {
+    PIP_ADAPTER_ADDRESSES p_addresses = NULL;
+    unsigned long out_buf_len = 0;
+    int dw_ret_val = 0;
     auto flags = GAA_FLAG_SKIP_ANYCAST & GAA_FLAG_SKIP_MULTICAST & GAA_FLAG_SKIP_DNS_SERVER;
 
     // Получить необходимое место для адреса 
-    dwRetVal = GetAdaptersAddresses(AF_INET,
+    dw_ret_val = GetAdaptersAddresses(AF_INET,
         flags,
         NULL,
         NULL,
-        &outBufLen);
+        &out_buf_len);
 
     // Выделить память для адресов
-    pAddresses = (PIP_ADAPTER_ADDRESSES)malloc(outBufLen);
+    p_addresses = (PIP_ADAPTER_ADDRESSES)malloc(out_buf_len);
 
     // Получаем таблицу интерфейсов
-    dwRetVal = GetAdaptersAddresses(AF_INET,
+    dw_ret_val = GetAdaptersAddresses(AF_INET,
         flags,
         NULL,
-        pAddresses,
-        &outBufLen);
+        p_addresses,
+        &out_buf_len);
 
-    if (dwRetVal != ERROR_SUCCESS) {
-        free(pAddresses);
-        throw std::runtime_error("Unable to get adapters table: " + std::to_string(dwRetVal));
+    if (dw_ret_val != ERROR_SUCCESS) {
+        free(p_addresses);
+        throw std::runtime_error("Unable to get adapters table: " + std::to_string(dw_ret_val));
     }
 
     // Пройтись по всем интерфейсам
-    PIP_ADAPTER_ADDRESSES pCurrAddresses = pAddresses;
-    while (pCurrAddresses) {
+    PIP_ADAPTER_ADDRESSES p_curr_addresses = p_addresses;
+    while (p_curr_addresses) {
         // Если индекс совпадает с искомым
-        if (pCurrAddresses->IfIndex == dwIndex) {
-            PIP_ADAPTER_UNICAST_ADDRESS pUnicast = pCurrAddresses->FirstUnicastAddress;
+        if (p_curr_addresses->IfIndex == dwIndex) {
+            PIP_ADAPTER_UNICAST_ADDRESS p_unicast = p_curr_addresses->FirstUnicastAddress;
             int i = 0;
 
-            while (pUnicast != NULL) {
+            while (p_unicast != NULL) {
                 char result[20] = {};
 
-                if (pUnicast->Address.lpSockaddr->sa_family == AF_INET) {
+                if (p_unicast->Address.lpSockaddr->sa_family == AF_INET) {
                     // Скопировать IPv4 адрес и вернуть его
-                    sockaddr_in* sa_in = (sockaddr_in*)pUnicast->Address.lpSockaddr;
+                    sockaddr_in* sa_in = (sockaddr_in*)p_unicast->Address.lpSockaddr;
                     inet_ntop(AF_INET, &(sa_in->sin_addr), result, sizeof(result));
-                    free(pAddresses);
+                    free(p_addresses);
 
                     return std::string(result, result + sizeof(result));
                 }
 
-                pUnicast = pUnicast->Next;
+                p_unicast = p_unicast->Next;
             }
 
             break;
         }
 
-        pCurrAddresses = pCurrAddresses->Next;
+        p_curr_addresses = p_curr_addresses->Next;
     }
 
-    free(pAddresses);
+    free(p_addresses);
     return "";
 }
 
@@ -157,6 +157,62 @@ std::ostream& ARPHelper::printARPTable(std::ostream& out) {
 	return out;
 }
 
-void ARPHelper::addArpEntry(std::string ip, std::string address, int interface_index) {
+MIB_IPNET_ROW2 constructArpRow(std::string ip, std::string address, int interface_index) {
+    // Инициализировать запись в ARP таблице
+    MIB_IPNET_ROW2 entry = {};
 
+    // Скопировать IP адрес в запись ARP таблицы
+    entry.Address.si_family = AF_INET;
+    inet_pton(AF_INET, &address[0], &entry.Address.Ipv4.sin_addr);
+
+    // Скопировать MAC адрес в запись ARP таблицы
+    entry.PhysicalAddressLength = 6;
+    sscanf_s(&address[0], "%02X:%02X:%02X:%02X:%02X:%02X", 
+        &entry.PhysicalAddress[0],
+        &entry.PhysicalAddress[1], 
+        &entry.PhysicalAddress[2], 
+        &entry.PhysicalAddress[3], 
+        &entry.PhysicalAddress[4], 
+        &entry.PhysicalAddress[5]);
+
+    // Скопировать индекс интерфейса в запись ARP таблицы
+    entry.InterfaceIndex = interface_index;
+
+    return entry;
+}
+
+void ARPHelper::addArpEntry(std::string ip, std::string address, int interface_index) {
+    // Сконструировать запись для ARP таблицы
+    auto new_entry = constructArpRow(ip, address, interface_index);
+
+    // Добавить запись
+    int return_code;
+    if ((return_code = CreateIpNetEntry2(&new_entry)) != NO_ERROR)
+        throw std::runtime_error("Unable to add entry with error code " + std::to_string(return_code));
+} 
+
+void ARPHelper::deleteArpEntry(std::string ip, std::string address, int interface_index) {
+    // Сконструировать запись для ARP таблицы
+    auto new_entry = constructArpRow(ip, address, interface_index);
+
+    // Удалить запись
+    int return_code;
+    if ((return_code = DeleteIpNetEntry2(&new_entry)) != NO_ERROR)
+        throw std::runtime_error("Unable to add entry with error code " + std::to_string(return_code));
+}
+
+std::string ARPHelper::sendARP(std::string find) {
+    unsigned long size = sizeof(unsigned long) * 2;
+    unsigned char mac[sizeof(unsigned long) * 2];
+    memset(mac, 0xff, sizeof(mac));
+    unsigned long ip;
+    inet_pton(AF_INET, &find[0], &ip);
+
+    // Отправить ARP запрос
+    int return_code;
+    if ((return_code = SendARP((IPAddr) &ip, ADDR_ANY, mac, &size)) == NO_ERROR)
+        throw std::runtime_error("Unable to add entry with error code " + std::to_string(return_code));
+
+    // Преобразовать адрес в строку
+    return ARPHelper::prettyPrintPhysicalAddress(mac, size);
 }

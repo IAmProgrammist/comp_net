0cb87051e5cbe46ca934167065d5a43ebc6ac39a
IAmProgrammist <vladislav.pakhomov@bk.ru>
2025-04-29 00:46:30 +0300

    Lab 6 WIP

---
+++ lab6/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <algorithm>
+#include <ws2tcpip.h>
+#include <webstur/dhcp.h>
+
+int main() {
+	try {
+		DHCPHelper::init();
+		std::string input;
+
+		// Считытваем команду пользователя
+		while (true)
+		{
+			std::cout 
+				<< "Enter T to get IP adress from domain name\n"
+				<< "Enter A to get domain name from IP address\n"
+				<< "Enter D to print IP address of DHCP server\n"
+				<< "Enter S to renew IP address\n"
+				<< "Enter E to exit loop"
+				<< std::endl;
+
+			std::cin >> input;
+			std::cin.get();
+			std::transform(input.begin(), input.end(), input.begin(), toupper);
+			if (input == "T") {
+				// Ввести имя домена
+				std::cout << "Enter domain name: ";
+				std::cout.flush();
+				std::string name;
+				std::getline(std::cin, name);
+
+				// Получить IP адрес
+				auto result = DHCPHelper::getAddrInfo(name);
+
+				char buf[64];
+				InetNtopA(AF_INET, &((sockaddr_in *) result->ai_addr)->sin_addr, buf, sizeof(buf));
+
+				std::cout << "IP address: " << std::string(buf, buf + strlen(buf)) << std::endl;
+			}
+			else if (input == "A") {
+				// Ввести IP адрес
+				sockaddr_in info;
+				info.sin_family = AF_INET;
+				std::cout << "Enter IP address: ";
+				std::cout.flush();
+				std::string name;
+				std::getline(std::cin, name);
+
+				char buf[64] = {};
+				memcpy(buf, name.c_str(), name.size());
+				InetPtonA(AF_INET, buf, &info.sin_addr);
+
+				DHCPHelper::getNameInfo(info);
+			}
+			else if (input == "D") {
+				// Вывести информацию о сервере
+				DHCPHelper::printDHCPServerInfo(std::cout);
+			}
+			else if (input == "S") {
+				// Обновить информацию об IP
+				DHCPHelper::renewIP();
+			}
+			else if (input == "E") {
+				// Выход из цикла
+				break;
+			}
+		}
+	}
+	catch (const std::runtime_error& error) {
+		std::cerr << "Failed while running server. Caused by: '" << error.what() << "'" << std::endl;
+
+		return -1;
+	}
+
+	// Выгрузка библиотеки WSA
+	DHCPHelper::detach();
+
+	return 0;
+}
---
+++ src/webstur/dhcp.cpp
@@ -0,0 +1,98 @@
+#include "pch.h"
+#include <iostream>
+#include <dhcpcsdk.h>
+#include <iphlpapi.h>
+#include <ws2tcpip.h>
+#include <webstur/dhcp.h>
+
+void DHCPHelper::init() {
+    std::clog << "Loading WSA library" << std::endl;
+    loadWSA();
+
+    std::clog << "Loading DHCP library" << std::endl;
+    DWORD dwVersion;
+    if (DhcpCApiInitialize(&dwVersion) != 0) {
+        throw std::invalid_argument(getErrorTextWithWSAErrorCode("Unable to load DHCP library"));
+    }
+}
+
+void DHCPHelper::detach() {
+    std::clog << "Unloading WSA library" << std::endl;
+    unloadWSA();
+
+    std::clog << "Unloading DHCP library" << std::endl;
+    DhcpCApiCleanup();
+}
+
+addrinfo* DHCPHelper::getAddrInfo(std::string name) {
+    addrinfo* res = nullptr;
+
+    std::clog << "Getting addr info" << std::endl;
+    if (getaddrinfo(name.c_str(), nullptr, nullptr, &res) != 0)
+        throw std::invalid_argument(getErrorTextWithWSAErrorCode("Unable to get addr info"));
+
+    return res;
+}
+
+std::string DHCPHelper::getNameInfo(addrinfo ip) {
+    char host[512];
+
+    std::clog << "Getting name info" << std::endl;
+    if (getnameinfo(ip.ai_addr, ip.ai_addrlen, host, sizeof(host), nullptr, 0, 0) != 0)
+        throw std::invalid_argument(getErrorTextWithWSAErrorCode("Unable to get name info"));
+
+    return std::string(host, host + strlen(host));
+}
+
+void DHCPHelper::printDHCPServerInfo(std::ostream& out) {
+    std::clog << "Getting interface info" << std::endl;
+    ULONG ulOutBufLen = 0;
+    PIP_ADAPTER_INFO pInfo = (PIP_ADAPTER_INFO) malloc(sizeof(IP_ADAPTER_INFO));
+    PIP_ADAPTER_INFO pAdapter;
+    if (GetAdaptersInfo(NULL, &ulOutBufLen) != NO_ERROR) {
+        free(pInfo);
+        pInfo = (PIP_ADAPTER_INFO) malloc(ulOutBufLen);
+    }
+    if (GetAdaptersInfo(pInfo, &ulOutBufLen) != NO_ERROR) {
+        free(pInfo);
+        throw std::invalid_argument(getErrorTextWithWSAErrorCode("Can't get interface info"));
+    }
+    
+    for (pAdapter = pInfo; pAdapter != NULL; pAdapter = pAdapter->Next) {
+        if (pAdapter->DhcpEnabled) {
+            out << pAdapter->DhcpServer.IpAddress.String << std::endl;
+        
+            free(pInfo);
+            return;
+        }
+    }
+
+    free(pInfo);
+}
+
+void DHCPHelper::renewIP() {
+    std::clog << "Getting interface info" << std::endl;
+    ULONG ulOutBufLen = 0;
+    PIP_INTERFACE_INFO pInfo;
+    pInfo = (IP_INTERFACE_INFO*) malloc(sizeof(IP_INTERFACE_INFO));
+    if (GetInterfaceInfo(NULL, &ulOutBufLen) != NO_ERROR) {
+        free(pInfo);
+        pInfo = (IP_INTERFACE_INFO*) malloc(ulOutBufLen);
+    }
+    if (GetInterfaceInfo(pInfo, &ulOutBufLen) != NO_ERROR) {
+        free(pInfo);
+        throw std::invalid_argument(getErrorTextWithWSAErrorCode("Can't get interface info"));
+    }
+
+    std::clog << "Resetting DHCP settings" << std::endl;
+    if (IpReleaseAddress(&pInfo->Adapter[0]) != NO_ERROR)
+        std::cerr << getErrorTextWithWSAErrorCode("Can't reset ip address") << std::endl;
+
+    if (IpRenewAddress(&pInfo->Adapter[0]) != NO_ERROR)
+        std::cerr << getErrorTextWithWSAErrorCode("Can't reset ip address") << std::endl;
+
+    if (pInfo != NULL) 
+        free(pInfo);
+
+    return;
+}
